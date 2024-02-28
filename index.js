bd5d33bd37773121c9a6e2fb640d841a4393e43f
Amal Mohammed <157481527+amolllat@users.noreply.github.com>
2024-02-28 19:53:02 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+// code solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+ console.log(saturdayFun());
+ console.log(saturdayFun("bathe my dog"));
+
+
+
+let mondayWork=function(activity='go to the office') {
+   return(`This Monday, I will ${activity}.`);
+};
+console.log(mondayWork());
+console.log(mondayWork("work from home"));
+
+
+
+function wrapAdjective(result='*') {
+    return function(emphatic ="a hard worker") {
+    return `You are ${result}${emphatic}${result}!`;
+    }
+}
+
+console.log(wrapAdjective()());
+
+let wrappedFunction = wrapAdjective("||");
+console.log(wrappedFunction("dedicated programmer"));
+
+
